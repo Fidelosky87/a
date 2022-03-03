d1e32bee9e6796bcef88efc2d33340a221457aed
Fidelosky87 <100180411+Fidelosky87@users.noreply.github.com>
2022-03-03 01:50:58 +0100

    Update MoodleClient.py

--- MoodleClient.py
+++ MoodleClient.py
@@ -11,7 +11,7 @@ class MoodleClient(object):
         self.username = user
         self.password = passw
         self.session = requests.Session()
-        self.path = 'http://www.aula.scu.sld.cu/'
+        self.path = 'http://aula.scu.sld.cu/'
         self.userdata = ''
 
     def getsession(self):
