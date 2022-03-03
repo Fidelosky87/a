a33475a88741a3fd154dcf7abc0fabc930d07964
Fidelosky87 <100180411+Fidelosky87@users.noreply.github.com>
2022-03-03 01:49:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from MoodleClient import MoodleClient
 BOT_TOKEN = '5214968075:AAENh9HXMsmCkwKndVa0InCsnuDHwvss1tU'
 MAX_ZIP_SIZE = 100
 ACCES_USERS = ['Fidelosky87','Fatal_Error_Lefty']
-CREDENTIALS = {'username':'Raquellilianaor','password':'Fredi@87'}
+CREDENTIALS = {'username':'fnafplus2','password':'Fredi@87'}
 CACHE = {}
 
 def getCache():
